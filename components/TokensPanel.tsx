669b51094e34ed6f27ffad486e67c3243bbc8aa2
Roamin <roamin@tuta.io>
2022-12-16 19:29:10 -0500

    add tokens panel

---
+++ components/TokensPanel.tsx
@@ -0,0 +1,32 @@
+import { Stack } from '@chakra-ui/react'
+import { useNetworks } from '../context/NetworksProvider'
+import { useTokens } from '../context/TokensProvider'
+import { useWallets } from '../context/WalletsProvider'
+import { TokenBalance } from './TokenBalance'
+
+
+export function TokensPanel() {
+
+  const { selectedAccount } = useWallets()
+  const { selectedNetwork } = useNetworks()
+  const { tokens } = useTokens()
+
+
+  return (
+    <Stack mt='6' spacing='3'>
+      {
+        selectedAccount && selectedNetwork && tokens.map(token => {
+          if (token.chainId === selectedNetwork.chainId) {
+            return (
+              <TokenBalance
+                key={`${token.address}`}
+                accountAddress={selectedAccount.account.public.address}
+                token={token}
+              />
+            )
+          }
+        })
+      }
+    </Stack>
+  )
+}
