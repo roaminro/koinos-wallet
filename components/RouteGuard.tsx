42778a669cba32c37072cd30b323ac477b588018
Roamin <roamin@tuta.io>
2022-12-22 18:39:11 -0500

    RouteGuard cleanup and improvements

--- components/RouteGuard.tsx
+++ components/RouteGuard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, ReactNode, useCallback } from 'react'
+import React, { ReactNode } from 'react'
 import { useRouter } from 'next/router'
 
 import { PUBLIC_PATHS } from '../util/Constants'
@@ -11,40 +11,13 @@ export default function RouteGuard({
 }): JSX.Element {
   const router = useRouter()
   const { isLocked, isLoading } = useWallets()
-  // const [authorized, setAuthorized] = useState(false)
-
-  // const authCheck = useCallback((url: string) => {
-  //   const path = url.split('?')[0]
-  //   setAuthorized(!isLocked || PUBLIC_PATHS.includes(path))
-  // }, [isLocked])
-
-  // useEffect(() => {
-  //   const hideContent = () => setAuthorized(false)
-
-  //   const setup = async () => {
-  //     // on route change start - hide page content by setting authorized to false  
-  //     router.events.on('routeChangeStart', hideContent)
-
-  //     // on route change complete - run auth check 
-  //     router.events.on('routeChangeComplete', authCheck)
-
-  //     // on initial load - run auth check 
-  //     authCheck(router.asPath)
-  //   }
-
-  //   setup()
-
-  //   // unsubscribe from events in useEffect return function
-  //   return () => {
-  //     router.events.off('routeChangeStart', hideContent)
-  //     router.events.off('routeChangeComplete', authCheck)
-  //   }
-  // }, [authCheck, router.asPath, router.events])
+  
+  const path = router.asPath.split('?')[0]
+  const authorized = !isLocked || PUBLIC_PATHS.includes(path)
 
   return (
     <>
-      {/* {authorized && children} */}
-      {!isLoading && children}
+      {!isLoading && authorized && children}
     </>
   )
 }
