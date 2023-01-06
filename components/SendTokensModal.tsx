6fa3f9974f6c6725eca9715459282a8f24ab8ee1
Roamin <roamin@tuta.io>
2023-01-06 14:04:10 -0500

    add `AutoCompleteInput` for the recipient send tokens modal

--- components/SendTokensModal.tsx
+++ components/SendTokensModal.tsx
@@ -1,7 +1,16 @@
-import { Text, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Button, FormControl, FormErrorMessage, FormHelperText, FormLabel, Input, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Select, useToast, Link, Tooltip } from '@chakra-ui/react'
+import { Text, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Button, FormControl, FormErrorMessage, FormHelperText, FormLabel, Input, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Select, useToast, Link, Tooltip, Stack } from '@chakra-ui/react'
 import { Contract, utils, Signer } from 'koilib'
 import { ChangeEvent, useEffect, useState } from 'react'
 import NiceModal, { useModal } from '@ebay/nice-modal-react'
+import {
+  AutoComplete,
+  AutoCompleteGroup,
+  AutoCompleteGroupTitle,
+  AutoCompleteInput,
+  AutoCompleteItem,
+  AutoCompleteList,
+  AutoCompleteCreatable
+} from '@choc-ui/chakra-autocomplete'
 import { useNetworks } from '../context/NetworksProvider'
 import { useWallets } from '../context/WalletsProvider'
 import { TransactionJson } from 'koilib/lib/interface'
@@ -20,7 +29,7 @@ export default NiceModal.create(({ defaultTokenAddress }: SendTokensModalProps) 
   const toast = useToast()
   const { mutate } = useSWRConfig()
 
-  const { selectedAccount, signTransaction } = useWallets()
+  const { wallets, selectedAccount, signTransaction } = useWallets()
   const { selectedNetwork, provider } = useNetworks()
   const { tokens } = useTokens()
 
@@ -159,57 +168,88 @@ export default NiceModal.create(({ defaultTokenAddress }: SendTokensModalProps) 
         <ModalHeader>Send tokens</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-
-          <FormControl>
-            <FormLabel>Token</FormLabel>
-            <Select
-              value={selectedToken?.address}
-              onChange={handleTokenChange}
-            >
-              {
-                availableTokens && Object.keys(availableTokens).map((tknAddr) => (
-                  <option key={tknAddr} value={tknAddr} >{availableTokens[tknAddr].name} ({availableTokens[tknAddr].symbol})</option>
-                ))
-              }
-            </Select>
-            <FormHelperText>Select the token to send.</FormHelperText>
-          </FormControl>
-
-          <FormControl>
-            <FormLabel>Sender</FormLabel>
-            <Input value={selectedAccount?.account.public.address} isDisabled={true} isReadOnly={true} />
-            <FormHelperText>The address of the sender.</FormHelperText>
-          </FormControl>
-
-          <FormControl isRequired isInvalid={isRecipientAddressInvalid}>
-            <FormLabel>Recipient</FormLabel>
-            <Input value={recipientAddress} onChange={handleRecipientAddressChange} />
-            <FormHelperText>The address of the recipient.</FormHelperText>
-            {
-              isRecipientAddressInvalid && <FormErrorMessage>The recipient address entered is invalid.</FormErrorMessage>
-            }
-          </FormControl>
-
-          <FormControl>
-            <FormLabel>Amount</FormLabel>
-            <NumberInput min={0} precision={selectedToken?.decimals} value={amount} onChange={handleAmountChange}>
-              <NumberInputField />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
-            <FormHelperText>
-              <Tooltip
-                label='set amount with balance'
-                placement="bottom"
-                hasArrow
+          <Stack>
+            <FormControl>
+              <FormLabel>Token</FormLabel>
+              <Select
+                value={selectedToken?.address}
+                onChange={handleTokenChange}
               >
-                <Link onClick={() => setAmount(formattedBalance)}>Balance: {formattedBalance}</Link>
-              </Tooltip>
-              <Text>Amount of tokens to send.</Text>
-            </FormHelperText>
-          </FormControl>
+                {
+                  availableTokens && Object.keys(availableTokens).map((tknAddr) => (
+                    <option key={tknAddr} value={tknAddr} >{availableTokens[tknAddr].name} ({availableTokens[tknAddr].symbol})</option>
+                  ))
+                }
+              </Select>
+              <FormHelperText>Select the token to send.</FormHelperText>
+            </FormControl>
+
+            <FormControl>
+              <FormLabel>Sender</FormLabel>
+              <Input value={selectedAccount?.account.public.address} isDisabled={true} isReadOnly={true} />
+              <FormHelperText>The address of the sender.</FormHelperText>
+            </FormControl>
+
+            <FormControl isRequired isInvalid={isRecipientAddressInvalid}>
+              <FormLabel>Recipient</FormLabel>
+              <AutoComplete
+              restoreOnBlurIfEmpty={false}
+                openOnFocus
+                emptyState={<Text align='center'>no recipient found</Text>}
+                onSelectOption={(selection) => {
+                  setRecipientAddress(selection.item.value)
+                }}
+              >
+                <AutoCompleteInput
+                  placeholder='Choose from wallets accounts...'
+                />
+                <AutoCompleteList>
+                  {Object.entries(wallets).map(([walletId, wallet], _) => (
+                    <AutoCompleteGroup key={walletId} showDivider>
+                      <AutoCompleteGroupTitle>
+                        {wallet.name}
+                      </AutoCompleteGroupTitle>
+                      {Object.entries(wallet.accounts).map(([accountId, account], _) => (
+                        <AutoCompleteItem
+                          key={accountId}
+                          value={account.public.address}
+                          label={account.public.name}
+                        >
+                          {account.public.name}
+                        </AutoCompleteItem>
+                      ))}
+                    </AutoCompleteGroup>
+                  ))}
+                </AutoCompleteList>
+              </AutoComplete>
+              <Input value={recipientAddress} onChange={handleRecipientAddressChange} />
+              <FormHelperText>The address of the recipient.</FormHelperText>
+              {
+                isRecipientAddressInvalid && <FormErrorMessage>The recipient address entered is invalid.</FormErrorMessage>
+              }
+            </FormControl>
+
+            <FormControl>
+              <FormLabel>Amount</FormLabel>
+              <NumberInput min={0} precision={selectedToken?.decimals} value={amount} onChange={handleAmountChange}>
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+              <FormHelperText>
+                <Tooltip
+                  label='set amount with balance'
+                  placement="bottom"
+                  hasArrow
+                >
+                  <Link onClick={() => setAmount(formattedBalance)}>Balance: {formattedBalance}</Link>
+                </Tooltip>
+                <Text>Amount of tokens to send.</Text>
+              </FormHelperText>
+            </FormControl>
+          </Stack>
         </ModalBody>
 
         <ModalFooter>
